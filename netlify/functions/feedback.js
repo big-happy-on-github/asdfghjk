27de20bcf6ccfa8e38f244d453136a405695e617
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-07-23 12:58:08 -0400

    Update feedback.js

--- netlify/functions/feedback.js
+++ netlify/functions/feedback.js
@@ -1,17 +1,25 @@
 let feedback = JSON.parse("{}");
 
 exports.handler = async event => {
+    if (event.httpMethod == "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify(feedback)
+        };
+    }
+
     const id = event.queryStringParameters.id;
     const requestBody = JSON.parse(event.body);
 
+    // Assuming feedback is defined somewhere
     if (!feedback[id]) {
         feedback[id] = [];
     }
 
-    feedback[id].append(requestBody);
+    feedback[id].push(requestBody); // Use push() instead of append()
 
     return {
         statusCode: 200,
-        body: "Thx")
+        body: "Thx"
     };
 };
